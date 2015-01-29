9ae829f964f3d91925b4213340e1eb44e7e33bda
robbiekenny <robert_kenny@outlook.com>
2015-01-29 12:37:10 +0000

    this is testing the commiting of java code

---
+++ week1/demo/Example.java
@@ -0,0 +1,35 @@
+package demo;
+
+public class Example {
+
+	private string name;
+	private int age;
+
+	public int getAge() {
+		return this.age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public Example() {
+		// TODO - implement Example.Example
+		throw new UnsupportedOperationException();
+	}
+
+	public void getName() {
+		// TODO - implement Example.getName
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param name
+	 */
+	public void setName(int name) {
+		// TODO - implement Example.setName
+		throw new UnsupportedOperationException();
+	}
+
+}
